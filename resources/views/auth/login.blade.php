37e15fc682556e89dea01a9d99d670944655f2e8
hakim123456 <messoudi.hakim@gmail.com>
2023-05-16 23:15:44 +0100

    mise a jour et traduction de auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="h3 mb-3 font-weight-normal">{{ __('تسجيل الدخول') }}</div>
+                <div class="card-header"> {{  __(' تسجيل الدخول ')  }} </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
